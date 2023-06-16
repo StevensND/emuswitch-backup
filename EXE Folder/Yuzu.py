d1883d49a261942aae6d1f806aec06b47f72f6be
Stevens <45856578+StevensND@users.noreply.github.com>
2023-06-16 14:08:13 +0200

    Update Yuzu.py

--- EXE Folder/Yuzu.py
+++ EXE Folder/Yuzu.py
@@ -1,75 +1,75 @@
-import sys
-from pathlib import Path
 import os
 import shutil
 import zipfile
 import tkinter as tk
 from tkinter import filedialog
-
-def resolve_path(path: str | Path) -> Path:
-    if getattr(sys, "frozen", False):
-        # If the script is running as a bundled app (.exe)
-        script_dir = Path(sys.executable).resolve().parent
-    else:
-        # If the script is running in development mode
-        script_dir = Path(__file__).resolve().parent
-
-    config_folder = script_dir.parent / 'config'
-    resolved_path = config_folder / path
-    return resolved_path
+import subprocess
 
 def open_qt_config():
-    # Locate "qt-config.ini" file in the "config" folder
-    qt_config_file = resolve_path("qt-config.ini")
-
-    # Check if "qt-config.ini" file exists
-    if not os.path.isfile(qt_config_file):
-        print("No 'qt-config.ini' file found in the 'config' folder.")
+    # Prompt the user to select the base directory where the search should start
+    root = tk.Tk()
+    root.withdraw()
+    base_directory = filedialog.askdirectory(title="Select base directory")
+    if not base_directory:
+        print("No base directory selected.")
         input("Press Enter to exit.")
         return
-
-    # Read nand_directory from qt-config.ini
-    with open(qt_config_file, 'r') as file:
-        for line in file:
-            if line.startswith('nand_directory='):
-                input_folder = line.replace('nand_directory=', '').strip()
-                # Append additional information to the input_folder path
-                input_folder = os.path.join(input_folder, 'user', 'save')
-                break
-        else:
-            print("No 'nand_directory' entry found in 'qt-config.ini'.")
-            input("Press Enter to exit.")
-            return
 
     # Select output file
     output_file = filedialog.asksaveasfilename(title="Select output file", defaultextension=".zip", filetypes=(("Zip Files", "*.zip"),))
 
     if not output_file:
         print("No output file location or name selected.")
+        input("Press Enter to exit.")
         return
 
     # Create a .zip file in write mode
     with zipfile.ZipFile(output_file, "w", compression=zipfile.ZIP_DEFLATED) as zipf:
-        # Traverse files and folders in the input folder
-        for root_folder, subfolders, filenames in os.walk(input_folder):
-            # Get the relative path of the current folder
-            relative_folder = os.path.relpath(root_folder, input_folder)
+        # Traverse files and folders in the base directory and its subdirectories
+        for root_folder, _, filenames in os.walk(base_directory):
+            # Check if "config" folder exists in the current folder
+            config_folder = os.path.join(root_folder, 'config')
+            if not os.path.exists(config_folder):
+                continue
 
-            # Exclude the "cache" folder from being included in the .zip file
-            if 'cache' in subfolders:
-                subfolders.remove('cache')
+            # Locate "qt-config.ini" file in the "config" folder
+            qt_config_file = os.path.join(config_folder, "qt-config.ini")
 
-            # Traverse files in the current folder
-            for filename in filenames:
-                # Build the source and destination paths for each file
-                input_file = os.path.join(root_folder, filename)
-                relative_file = os.path.join(relative_folder, filename)
+            # Check if "qt-config.ini" file exists
+            if not os.path.isfile(qt_config_file):
+                continue
 
-                # Add the file to the .zip file with the preserved relative path
-                zipf.write(input_file, arcname=relative_file)
+            # Read nand_directory from qt-config.ini
+            with open(qt_config_file, 'r') as file:
+                for line in file:
+                    if line.startswith('nand_directory='):
+                        input_folder = line.replace('nand_directory=', '').strip()
+                        # Append additional information to the input_folder path
+                        input_folder = os.path.join(input_folder, 'user', 'save')
+                        break
+                else:
+                    print(f"No 'nand_directory' entry found in 'qt-config.ini' of {config_folder}.")
+                    continue
+
+            # Traverse files and folders in the input folder
+            for root, _, files in os.walk(input_folder):
+                # Get the relative path of the current folder
+                relative_folder = os.path.relpath(root, input_folder)
+
+                # Exclude the "cache" folder from being included in the .zip file
+                if 'cache' in _:
+                    _.remove('cache')
+
+                # Traverse files in the current folder
+                for filename in files:
+                    # Build the source and destination paths for each file
+                    input_file = os.path.join(root, filename)
+                    relative_file = os.path.join(relative_folder, filename)
+
+                    # Add the file to the .zip file with the preserved relative path
+                    zipf.write(input_file, arcname=relative_file)
 
     print("Compressed file copy created at:", output_file)
-
-if __name__ == "__main__":
-    open_qt_config()
     input("Press Enter to exit.")
+
+open_qt_config()
